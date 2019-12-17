7cdcb81a9189e5ca42e82b55d94afe21a89ae01e
tehilauzan <46137999+tehilauzan@users.noreply.github.com>
2019-12-17 15:16:09 +0200

    start graph project

---
+++ src/algorithms/Graph_Algo.java
@@ -0,0 +1,65 @@
+package algorithms;
+
+import java.util.List;
+import dataStructure.graph;
+import dataStructure.node_data;
+	
+/**
+ * This empty class represents the set of graph-theory algorithms
+ * which should be implemented as part of Ex2 - Do edit this class.
+ * @author 
+ *
+*/
+public class Graph_Algo implements graph_algorithms{
+
+	@Override
+	public void init(graph g) 
+	{
+			
+	}
+
+	@Override
+	public void init(String file_name) 
+	{
+			
+	}
+
+	@Override
+	public void save(String file_name) 
+	{
+			
+	}
+
+	@Override
+	public boolean isConnected() 
+	{
+		return false;
+	}
+
+	@Override
+	public double shortestPathDist(int src, int dest) 
+	{
+		return 0;
+	}
+
+	@Override
+	public List<node_data> shortestPath(int src, int dest) 
+	{
+		return null;
+	}
+
+	@Override
+	public List<node_data> TSP(List<Integer> targets) 
+	{
+		return null;
+	}
+		
+	@Override
+	public graph copy() 
+	{
+		return null;
+	}
+		
+}
+		
+		
